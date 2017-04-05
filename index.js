23f50d82d07cc0344f538ded41285056c781855c
zivbrachia@gmail.com <zivbrachia@gmail.com>
2017-04-05 14:31:58 +0300

    fix

--- index.js
+++ index.js
@@ -1027,7 +1027,7 @@ function chatFlow(connObj, response, userData, source) {
         if (msg!==null) {
             messages.unshift(msg.toMessage());
         }
-    } else if ((intentAction==='input.explain' || intentAction==='input.right_reply') && (!!userData.study_session.stat.total_questions)) {
+    } else if ((intentAction==='input.explain' || intentAction==='input.right') && (!!userData.study_session.stat.total_questions)) {
         if (((Object.keys(userData.study_session.questions).length) === 1) && (!!userData.study_session.stat.total_questions)) {
             let msg = msgWithStudySessionStatImage(address, userData);
             messages.push(msg.toMessage());
